daf85c16864aa5cd9670ebd26609a5073ddeef42
wittig_reaction2 <wittig_reaction2@yahoo.co.jp>
2024-11-28 15:43:46 +0000

    added unused port demo

--- demo_file_context_resource_manager.py
+++ demo_file_context_resource_manager.py
@@ -3,9 +3,22 @@
 # safer way of managing file handles using a context resource maneger (with statement)
 import sys
 # New code
-
+import re
 #open file handle for reading in text mode
 with open(r"C:\Users\Kohno\project\Python_Labs_Nov26\movies.txt", mode = 'rt') as fin_out:
     for line in fin_out:
         print(line, end='')
+port_used = set()
+all_ports=set(range(1,201))
+with open(r"C:\Users\Kohno\Desktop\services", mode = 'rt') as fin_out:
+    for line in fin_out:
+        m=re.search(r'(\d+)/(tcp|udp)',line)
+        if m:
+            port = int(m.group(1)) 
+            if port <=200:
+                
+                port_used.add(m)
+print(port_used)
+        
+      
 
