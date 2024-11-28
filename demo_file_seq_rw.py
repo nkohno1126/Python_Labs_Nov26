248e3aac8620643a90275d23f3985576b97eafc0
wittig_reaction2 <wittig_reaction2@yahoo.co.jp>
2024-11-28 13:30:01 +0000

    add

--- demo_file_seq_rw.py
+++ demo_file_seq_rw.py
@@ -4,10 +4,25 @@
 import sys
 # New code
 
+movies = { 'chris': ['die hard', 'trainspotting', 'barbie'],
+           'tom': ['12 strong', 'forest gump', 'the matrix'],
+           'andrius': ['gladiator', 'the boondock saints', 'con air'],
+           'winson': ['top gun', 'terminator', 'pretty woman']
+}
 
 
 
+fin_out= open (r"C:\Users\Kohno\project\Python_Labs_Nov26\movies.txt", mode = 'rt')
 
+#text = fin_out.readlines()
+
+for line in fin_out:
+    print(line, end = '')
+
+
+#print(text)
+
+fin_out.close()
 
 
 sys.exit(0)
